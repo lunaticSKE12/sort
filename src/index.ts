4d924b2c4147f66e778367ec0ea0f4acead1f671
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-01-06 23:14:44 +0700

    LinkedList sort

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { LinkedList } from './LinkedList';
 import { CharacterCollection } from './CharactersCollection';
 import { NumberCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
@@ -7,7 +8,16 @@ import { Sorter } from './Sorter';
 // sorter.sort();
 // console.log(numberCollection.data);
 
-const characterCollection = new CharacterCollection('Cbasbca');
-const sorter = new Sorter(characterCollection);
+// const characterCollection = new CharacterCollection('Cbasbca');
+// const sorter = new Sorter(characterCollection);
+// sorter.sort();
+// console.log(characterCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(characterCollection.data);
+linkedList.print();
