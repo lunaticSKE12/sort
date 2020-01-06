47d2384eeed5c4d54d3befaacab150b233176b27
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-01-06 23:38:35 +0700

    Abstract class refactor

--- build/Sorter.js
+++ build/Sorter.js
@@ -2,15 +2,15 @@
 // import { NumberCollection } from './NumbersCollection';
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter = /** @class */ (function () {
-    function Sorter(collection) {
-        this.collection = collection;
+    function Sorter() {
     }
+    // constructor(public collection: Sortable) {}
     Sorter.prototype.sort = function () {
-        var length = this.collection.length;
+        var length = this.length;
         for (var i = 0; i < length; i++) {
             for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection.compare(j, j + 1)) {
-                    this.collection.swap(j, j + 1);
+                if (this.compare(j, j + 1)) {
+                    this.swap(j, j + 1);
                 }
             }
         }
--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,24 @@ import { CharacterCollection } from './CharactersCollection';
 import { NumberCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 
-// const numberCollection = new NumberCollection([50, 3, -5, 0]);
+const numberCollection = new NumberCollection([50, 3, -5, 0]);
 // const sorter = new Sorter(numberCollection);
 // sorter.sort();
-// console.log(numberCollection.data);
+numberCollection.sort();
+console.log(numberCollection.data);
 
-// const characterCollection = new CharacterCollection('Cbasbca');
+const characterCollection = new CharacterCollection('Cbasbca');
 // const sorter = new Sorter(characterCollection);
 // sorter.sort();
-// console.log(characterCollection.data);
+characterCollection.sort();
+console.log(characterCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
 linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(4);
-const sorter = new Sorter(linkedList);
-sorter.sort();
+// const sorter = new Sorter(linkedList);
+// sorter.sort();
+linkedList.sort();
 linkedList.print();
